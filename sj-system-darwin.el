9bcb7aedf92abf9b5040ce4d064a837420ddff2d
Sudish Joseph <sudish@gmail.com>
2009-03-16 18:25:45 -0400

    Better sizing and position for the initial emacs frame.

--- sj-system-darwin.el
+++ sj-system-darwin.el
@@ -3,8 +3,16 @@
 ;; Copyright: Sudish Joseph
 ;; Created: 2003-08-03
 
-;; Change the default font for all frames
-(push '(font . "-*-Monaco-*-120-*") default-frame-alist)
+;; Default and inital frame parameters
+(defconst sj/default-frame-parameters '((width . 80) (height . 50)
+					(font . "-*-Monaco-*-120-*"))
+  "Default frame parameters for all frames, including the initial one.")
+(let ((left-pos (/ (- (x-display-pixel-width) (frame-pixel-width)) 2)))
+  (setq initial-frame-alist (append `((top . 0) (left . ,left-pos))
+				    sj/default-frame-parameters
+				    initial-frame-alist)
+	default-frame-alist (append sj/default-frame-parameters
+				    default-frame-alist)))
 
 (setq ns-antialias-text t
       ns-option-modifier 'meta)
