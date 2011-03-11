66bf23bb62a7426a76c8c77f43f5503c520a058b
Sudish Joseph <sudish@gmail.com>
2011-03-11 01:16:01 -0500

    Turn on visual-line-mode globally.

--- sj-init.el
+++ sj-init.el
@@ -72,6 +72,14 @@
 ;; No tool bar, please
 (tool-bar-mode -1)
 
+;; Don't blink the cursor
+(blink-cursor-mode -1)
+
+;; Better display of long lines
+(setq visual-line-fringe-indicators	; always display wrap indicators
+      (cdr (assq 'continuation fringe-indicator-alist)))
+(global-visual-line-mode)
+
 ;; Tweak some of the defaults
 (setq track-eol 		t
       scroll-step 		0
