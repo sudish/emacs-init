71146a8fe5af448453a6de891c485904309d5061
Sudish Joseph <sudish@gmail.com>
2011-12-27 16:41:30 -0500

    Removed ffap, igrep & perspective modes.

--- sj-modes.el
+++ sj-modes.el
@@ -169,9 +169,6 @@ through paredit and highlight-paren")
 	       '("app" "config" "db" "lib" "script" "test")))))
   :relevant-files ("\\.rb$" "\\.yml$"))
 
-;; perspective: multiple workspaces
-(sj/load-path-prepend "external/perspective-el")
-(require 'perspective)
 
 ;; winner: rotate through window config history
 (winner-mode 1)
@@ -211,12 +208,6 @@ through paredit and highlight-paren")
 (require 'complete)
 (partial-completion-mode t)
 
-;; ffap: find file at point (now using ido)
-;; (require 'ffap)
-;; (ffap-bindings)
-;; (require 'ffap-url)
-;; (setq ffap-url-fetcher 'ffap-url-fetcher)
-
 ;; pabbrev
 ;; (require 'pabbrev)
 ;; (global-pabbrev-mode)
@@ -250,30 +241,6 @@ through paredit and highlight-paren")
 
 ;; sgml
 (setq sgml-quick-keys t)
-
-;; igrep -- a better interface to the greps
-;; (autoload 'igrep "igrep"
-;;   "*Run `grep` PROGRAM to match EXPRESSION in FILES..." t)
-;; (autoload 'igrep-find "igrep" "*Run `grep` via `find`..." t)
-;; (autoload 'igrep-visited-files "igrep"
-;;   "*Run `grep` ... on all visited files." t)
-;; (autoload 'dired-do-igrep "igrep"
-;;   "*Run `grep` on the marked (or next prefix ARG) files." t)
-;; (autoload 'dired-do-igrep-find "igrep"
-;;   "*Run `grep` via `find` on the marked (or next prefix ARG) directories." t)
-;; (autoload 'Buffer-menu-igrep "igrep"
-;;   "*Run `grep` on the files visited in buffers marked with '>'." t)
-;; (autoload 'igrep-insinuate "igrep"
-;;   "Define `grep' aliases for the corresponding `igrep' commands." t)
-;; (autoload 'grep "igrep"
-;;   "*Run `grep` PROGRAM to match EXPRESSION in FILES..." t)
-;; (autoload 'egrep "igrep" "*Run `egrep`..." t)
-;; (autoload 'fgrep "igrep" "*Run `fgrep`..." t)
-;; (autoload 'agrep "igrep" "*Run `agrep`..." t)
-;; (autoload 'grep-find "igrep" "*Run `grep` via `find`..." t)
-;; (autoload 'egrep-find "igrep" "*Run `egrep` via `find`..." t)
-;; (autoload 'fgrep-find "igrep" "*Run `fgrep` via `find`..." t)
-;; (autoload 'agrep-find "igrep" "*Run `agrep` via `find`..." t)
 
 ;; filladapt
 (require 'filladapt)
