01cb8feb68cd28cd317631c1de90c85bff442352
Sudish Joseph <sudish@gmail.com>
2011-12-27 16:34:32 -0500

    Keymap tweaks.

--- sj-keymaps.el
+++ sj-keymaps.el
@@ -7,25 +7,21 @@
 (define-key global-map [(super f1)]    'sj/tile-or-rotate-frames)
 (define-key global-map [(super A)]     'align-regexp)
 (define-key global-map [(super m)]     'magit-status)
-(define-key global-map [(super r)]     'rgrep)
-(define-key global-map [(super R)]     'sj/project-root-rgrep)
+(define-key global-map [(super r)]     'ack)
+(define-key global-map [(super R)]     'rgrep)
 (define-key global-map [(super t)]     'sj/anything-textmate)
 (define-key global-map [(super T)]     'sj/anything-code-nav)
 (define-key global-map [(super u)]     'undo)
 (define-key global-map [(super :)]     'eval-expression)
 (define-key global-map [(super \0)]    'delete-window)
 (define-key global-map [(super \1)]    'delete-other-windows)
-;; (define-key global-map [(super up)]    'windmove-up)
-;; (define-key global-map [(super down)]  'windmove-down)
-;; (define-key global-map [(super left)]  'windmove-left)
-;; (define-key global-map [(super right)] 'windmove-right)
-(define-key global-map [(super up)]
+(define-key global-map [(super left)]
   (defun sj/previous-window ()
     (interactive)
     (other-window -1)))
-(define-key global-map [(super down)]  'other-window)
-(define-key global-map [(super left)]  'winner-undo)
-(define-key global-map [(super right)] 'winner-redo)
+(define-key global-map [(super right)]  'other-window)
+(define-key global-map [(super down)]  'winner-undo)
+(define-key global-map [(super up)] 'winner-redo)
 (define-key global-map (kbd "C-;") 'iedit-mode)
 
 ;; private keymap for useful, but less commonly used stuff
@@ -34,11 +30,12 @@
 
 (define-key sj/private-keymap [(super .)] 'anything)
 
+(define-key sj/private-keymap [(super ?f)] 'follow-delete-other-windows-and-split)
 (define-key sj/private-keymap [?i] 'insert-buffer)
 (define-key sj/private-keymap [?l] 'load-library)
 (define-key sj/private-keymap [?L] 'locate-library)
 (define-key sj/private-keymap [?k] 'bury-buffer)
-(define-key sj/private-keymap [?m] 'compile)
+(define-key sj/private-keymap [(super ?m)] 'minimap-create)
 (define-key sj/private-keymap [?s] 'sj/swap-window-positions)
 
 ;; the default set-fill-column binding is annoying
